64237da2230b7231f5f9f2c077481ce60ec61093
Somesh Singh <someshsingh298@gmail.com>
2023-04-26 18:00:57 +0530

    numpy gone

--- yoga.py
+++ yoga.py
@@ -22,6 +22,22 @@ cv2.namedWindow('Pose Classification', cv2.WINDOW_NORMAL)
 
 timer = 0
 poses = []
+total_cals_burned = 0
+
+def calculate_calories_burnt(pred):
+    num_extended_limbs = sum([int(limb) for limb in pred.limbs])
+
+    left_knee = pred.keypoints.LEFT_KNEE
+    left_hip = pred.keypoints.LEFT_HIP
+    right_knee = pred.keypoints.RIGHT_KNEE
+    right_hip = pred.keypoints.RIGHT_HIP
+    left_leg_distance = abs(left_knee.y - left_hip.y)
+    right_leg_distance = abs(right_knee.y - right_hip.y)
+    avg_leg_distance = (left_leg_distance + right_leg_distance) / 2
+
+    calories_burnt = (num_extended_limbs * 5) + (avg_leg_distance * 0.1)
+
+    return calories_burnt
 
 while True:
     timer += 1
@@ -52,15 +68,19 @@ while True:
     if(timer % 5 == 0 and pred.pose != 'Unknown Pose'):
         poses.append(pred.pose)
 
+    cals_burned = calculate_calories_burnt(pred)
+
     color = (0, 0, 255)
     if(pred.pose != 'Unkown Pose'):
         color = (0, 255, 0)
+        total_cals_burned += cals_burned
 
+    
     cv2.putText(img, pred.pose, (10, 30),
                 cv2.FONT_HERSHEY_PLAIN, 2, color, 2)
     cv2.putText(img, 'Accuracy - ' + str(pred.accuracy_val), (10, 70),
                 cv2.FONT_HERSHEY_PLAIN, 2, color, 2)
-    cv2.putText(img, 'Total calories burned - ' + str(pred.total_cal_burned), (650, 30),
+    cv2.putText(img, 'Total calories burned - ' + str(total_cals_burned), (650, 30),
                 cv2.FONT_HERSHEY_PLAIN, 2, color, 2)
     cv2.putText(img, 'Total correct poses done - ' + str(len(poses)), (550, 70),
                 cv2.FONT_HERSHEY_PLAIN, 2, color, 2)
